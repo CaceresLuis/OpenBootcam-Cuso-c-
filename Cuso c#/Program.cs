1e97eea276abd1d32ae0b5a51fcc23c27f8111d7
Luis Caceres <56177103+CaceresLuis@users.noreply.github.com>
2023-05-08 13:33:23 -0600

    Add project files.

---
+++ Cuso c#/Program.cs
@@ -0,0 +1,22 @@
+﻿Clientee misCooerdenadas = new("Liz Villa", 74125896, "Usulutan, El salvador", "Lisvilla@gmail.com", true);
+Console.WriteLine(misCooerdenadas);
+
+public struct Clientee
+{
+    public Clientee(string nombreCompleto, int telefono, string direccion, string email, bool esNuevo)
+    {
+        NombreCompleto = nombreCompleto;
+        Telefono = telefono;
+        Direccion = direccion;
+        Email = email;
+        EsNUevo = esNuevo;
+    }
+
+    public string NombreCompleto { get; set; }
+    public int Telefono { get; set; }
+    public string Direccion { get; set; }
+    public string Email { get; set; }
+    public bool EsNUevo { get; set; }
+
+    public override string ToString() => $"Nombre completo: {NombreCompleto}, Telefono: {Telefono}, Direccion: {Direccion}, Email: {Email}, Es nuevo cliente?: {EsNUevo}";
+}
