b6b9c760af9e75c1cc87950276c8c5c38a94b9a8
Luis Caceres <56177103+CaceresLuis@users.noreply.github.com>
2023-05-08 15:06:01 -0600

    Ejercicios sesión 4

---
+++ Cuso c#/EjercicioSesion4.cs
@@ -0,0 +1,89 @@
+﻿namespace Cuso_c_
+{
+    public class EjercicioSesion4
+    {
+        public void Ejerccio1()
+        {
+            Console.Write("Ingresa un número entero: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            int multiplicador = 1;
+
+            while (multiplicador <= 10)
+            {
+                int resultado = numero * multiplicador;
+                Console.WriteLine($"{numero} x {multiplicador} = {resultado}");
+
+                multiplicador++;
+            }
+        }
+
+        public void Ejerccio2() 
+        {
+            int contadorPositivos = 0;
+            int contadorNegativos = 0;
+            string entrada;
+
+            do
+            {
+                Console.Write("Ingresa un número (o 'q' para salir): ");
+                entrada = Console.ReadLine();
+
+                if (int.TryParse(entrada, out int numero))
+                {
+                    if (numero > 0)
+                    {
+                        contadorPositivos++;
+                    }
+                    else if (numero < 0)
+                    {
+                        contadorNegativos++;
+                    }
+                }
+                else if (entrada.ToLower() != "q")
+                {
+                    Console.WriteLine("Entrada inválida. Intenta nuevamente.");
+                }
+            }
+            while (entrada.ToLower() != "q");
+
+            Console.WriteLine($"Cantidad de números positivos: {contadorPositivos}");
+            Console.WriteLine($"Cantidad de números negativos: {contadorNegativos}");
+        }
+
+        public void Ejercicio3()
+        {
+            Console.Write("Cantidad de figuras: ");
+            int rep = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nResultado:\n");
+
+            for (int r = 0; r < rep; r++)
+            {
+                Console.WriteLine($"Ingrese las dimensiones de la figura #{r+1}:");
+                Console.Write("Ancho: ");
+                int ancho = int.Parse(Console.ReadLine());
+                Console.Write("Alto: ");
+                int alto = int.Parse(Console.ReadLine());
+                Console.Write($"Relleno (0 para no, 1 para sí): ");
+                int relleno = int.Parse(Console.ReadLine());
+                for (int i = 0; i < alto; i++)
+                {
+                    for (int j = 0; j < ancho; j++)
+                    {
+                        if (relleno == 1 || i == 0 || i == alto - 1 || j == 0 || j == ancho - 1)
+                        {
+                            Console.Write("*");
+                        }
+                        else
+                        {
+                            Console.Write(" ");
+                        }
+                    }
+
+                    Console.WriteLine();
+                } 
+            }
+        }
+    }
+}
--- Cuso c#/Program.cs
+++ Cuso c#/Program.cs
@@ -1,22 +1,30 @@
-﻿Clientee misCooerdenadas = new("Liz Villa", 74125896, "Usulutan, El salvador", "Lisvilla@gmail.com", true);
-Console.WriteLine(misCooerdenadas);
+﻿
+using Cuso_c_;
 
-public struct Clientee
-{
-    public Clientee(string nombreCompleto, int telefono, string direccion, string email, bool esNuevo)
-    {
-        NombreCompleto = nombreCompleto;
-        Telefono = telefono;
-        Direccion = direccion;
-        Email = email;
-        EsNUevo = esNuevo;
-    }
+EjercicioSesion4 sesion4 = new EjercicioSesion4();
+sesion4.Ejerccio1();
+sesion4.Ejerccio2();
+sesion4.Ejercicio3();
 
-    public string NombreCompleto { get; set; }
-    public int Telefono { get; set; }
-    public string Direccion { get; set; }
-    public string Email { get; set; }
-    public bool EsNUevo { get; set; }
+//Clientee misCooerdenadas = new("Liz Villa", 74125896, "Usulutan, El salvador", "Lisvilla@gmail.com", true);
+//Console.WriteLine(misCooerdenadas);
 
-    public override string ToString() => $"Nombre completo: {NombreCompleto}, Telefono: {Telefono}, Direccion: {Direccion}, Email: {Email}, Es nuevo cliente?: {EsNUevo}";
-}
+//public struct Clientee
+//{
+//    public Clientee(string nombreCompleto, int telefono, string direccion, string email, bool esNuevo)
+//    {
+//        NombreCompleto = nombreCompleto;
+//        Telefono = telefono;
+//        Direccion = direccion;
+//        Email = email;
+//        EsNUevo = esNuevo;
+//    }
+
+//    public string NombreCompleto { get; set; }
+//    public int Telefono { get; set; }
+//    public string Direccion { get; set; }
+//    public string Email { get; set; }
+//    public bool EsNUevo { get; set; }
+
+//    public override string ToString() => $"Nombre completo: {NombreCompleto}, Telefono: {Telefono}, Direccion: {Direccion}, Email: {Email}, Es nuevo cliente?: {EsNUevo}";
+//}
